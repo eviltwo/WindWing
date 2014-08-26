bc23e5a8fbdce725f668b464d70b2f00a2e8040e
eviltwo <e135764@DaikiMacBookPro.local>
2014-08-27 00:14:18 +0900

    翼を生やした
    翼を作成。まだ飛ばない。
    感度が悪いコントローラだとカクカクする。

--- Assets/Script/WingController.cs
+++ Assets/Script/WingController.cs
@@ -6,16 +6,21 @@ public class WingController : MonoBehaviour {
 	public GameObject WingPartsPrefab;		// 翼パーツのプレハブ
 	public int PartsValue = 10;				// 片側あたりのパーツ数
 	public float WingWide = 3.0f;			// 翼の幅
+	public float PartsDist = 0.4f;
+	public float WingRotMax = 80.0f;
+	public float PartsRotMax = 5.0f;
 
 	int[] right = new int[]{1,-1};
+	GameObject[,] Parts;
 	// Use this for initialization
 	void Start () {
+		Parts = new GameObject[2,PartsValue];
 		createWing ();
 	}
 	
 	// Update is called once per frame
 	void Update () {
-	
+		moveWing ();
 	}
 
 	/// <summary>
@@ -30,6 +35,40 @@ public class WingController : MonoBehaviour {
 				Vector3 pos = Vector3.zero;
 				pos.x = WingWide / PartsValue * j * right [i];
 				parts.transform.localPosition = pos;
+				// 大きさ
+				Vector3 scale = parts.transform.localScale;
+				scale.x *= 1.2f * j;
+				parts.transform.localScale = scale;
+
+				Parts [i, j] = parts;
+			}
+		}
+	}
+
+	/// <summary>
+	/// 翼を制御
+	/// </summary>
+	void moveWing(){
+		// 入力
+		float[,] sticks = new float[2,2];
+		sticks [0, 0] = Input.GetAxis ("Horizontal_R1");
+		sticks [0, 1] = Input.GetAxis ("Vertical_R1");
+		sticks [1, 0] = Input.GetAxis ("Horizontal_L1");
+		sticks [1, 1] = Input.GetAxis ("Vertical_L1");
+
+		// 位置調整
+		for (int i = 0; i < 2; i++) {
+			GameObject temp = new GameObject ();
+			temp.transform.parent = transform;
+			temp.transform.localPosition = Vector3.zero;
+			temp.transform.Rotate (WingRotMax*sticks[i,1],(90-30)*right [i],0);
+			for (int j = 0; j < PartsValue; j++) {
+				GameObject parts = Parts[i,j];
+				parts.transform.position = temp.transform.position;
+				parts.transform.rotation = temp.transform.rotation;
+				parts.transform.position += parts.transform.right * (parts.transform.localScale.x/2) * right[i];
+				temp.transform.Rotate (-PartsRotMax*WingRotMax*(sticks[i,0]-1f),-3f*right[i],0);
+				temp.transform.position += temp.transform.forward * PartsDist;
 			}
 		}
 	}
